6aa541c4f9eacc3b76da1349e16f5f5ee8d4f1e4
Vincent Dubuc <cerise.litchis.0z@icloud.com>
2024-11-27 23:24:44 +0100

    Fix erreur id

--- src/main/java/polytech/service_reservations/repository/ReservationRepository.java
+++ src/main/java/polytech/service_reservations/repository/ReservationRepository.java
@@ -10,5 +10,5 @@ import polytech.service_reservations.model.Reservation;
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     List<Reservation> findByIdEmplacement(Long idEmplacement);
-    List<Reservation> findByIdClient(Long idClient);
+    List<Reservation> findByIdClient(Long idVoyageur);
 }
